2c896f1cddb470679baa003dfe773f71f64c0c5e
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2020-02-29 23:15:55 -0300

    Add files via upload

---
+++ precache-manifest.1e7ebd6f124f665064687464b632889b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d690aaec8c88b00bddb425160bd687a6",
+    "url": "/site-joao-azevedo/index.html"
+  },
+  {
+    "revision": "b1d3ab3b8b7150a52809",
+    "url": "/site-joao-azevedo/static/css/main.ff5fcdd2.chunk.css"
+  },
+  {
+    "revision": "8ced62f48dc0c0d8bc3f",
+    "url": "/site-joao-azevedo/static/js/2.42004ef8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/site-joao-azevedo/static/js/2.42004ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d3ab3b8b7150a52809",
+    "url": "/site-joao-azevedo/static/js/main.75bac918.chunk.js"
+  },
+  {
+    "revision": "f3685e4da9e8e233c4e7",
+    "url": "/site-joao-azevedo/static/js/runtime-main.f65e8d73.js"
+  },
+  {
+    "revision": "836ec7ff8a85788c9332f0c51390610d",
+    "url": "/site-joao-azevedo/static/media/background.836ec7ff.png"
+  },
+  {
+    "revision": "68866f038c78ada762504bf998fe2de0",
+    "url": "/site-joao-azevedo/static/media/helvetica-light.68866f03.ttf"
+  }
+]);
